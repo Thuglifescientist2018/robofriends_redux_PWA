cd3eeeb44c682b99657b1dadac1ce5f5e245aea7
Shashwat Dahal <thuglifescientist2018@gmail.com>
2025-06-18 16:41:25 +0545

    trying different api for next gen webp image format

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
   return (
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} height={200} width={200}  fetchPriority='high'/>
+      <img alt='robots' src={`https://api.dicebear.com/7.x/bottts-neutral/webp?seed=${id}&size=200
+`} height={200} width={200}  fetchPriority='high'/>
       <div>
         <h2 style={{margin: '0', fontSize: 18}}>{name}</h2>
         <p style={{margin: 0, fontSize: 15}}>{email}</p>
