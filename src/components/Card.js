f0bf5c72d8f31928e37de8f931281e31b97e49f9
Shashwat Dahal <thuglifescientist2018@gmail.com>
2025-06-18 13:21:00 +0545

    add favicon.ico and other icon manifest

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
   return (
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
+      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} height={200} width={200}  fetchPriority='high'/>
       <div>
-        <h2>{name}</h2>
-        <p>{email}</p>
+        <h2 style={{margin: '0', fontSize: 18}}>{name}</h2>
+        <p style={{margin: 0, fontSize: 15}}>{email}</p>
       </div>
     </div>
   );
